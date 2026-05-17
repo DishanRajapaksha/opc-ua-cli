e5b17f4ef6e0fc2d932b8e5d78b0020ff8fd1a76
DishanRajapaksha <hello@dishanrajapaksha.com>
2026-05-17 12:00:46 +0530

    Add output helpers

---
+++ internal/output/output.go
@@ -0,0 +1,52 @@
+package output
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+	"text/tabwriter"
+)
+
+const (
+	FormatTable = "table"
+	FormatText  = "text"
+	FormatJSON  = "json"
+)
+
+func NormaliseFormat(value string) string {
+	switch strings.ToLower(strings.TrimSpace(value)) {
+	case "", FormatTable:
+		return FormatTable
+	case FormatText:
+		return FormatText
+	case FormatJSON:
+		return FormatJSON
+	default:
+		return value
+	}
+}
+
+func WriteJSON(w io.Writer, value interface{}) error {
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "  ")
+	return encoder.Encode(value)
+}
+
+func WriteJSONLine(w io.Writer, value interface{}) error {
+	return json.NewEncoder(w).Encode(value)
+}
+
+func WriteTable(w io.Writer, headers []string, rows [][]string) error {
+	tw := tabwriter.NewWriter(w, 0, 0, 2, ' ', 0)
+	fmt.Fprintln(tw, strings.Join(headers, "\t"))
+	for _, row := range rows {
+		fmt.Fprintln(tw, strings.Join(row, "\t"))
+	}
+	return tw.Flush()
+}
+
+func WriteText(w io.Writer, value interface{}) error {
+	_, err := fmt.Fprintln(w, value)
+	return err
+}
