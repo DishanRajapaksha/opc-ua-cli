c8d415400cfa512f3725020f6bba6a5dbcabf5fc
DishanRajapaksha <hello@dishanrajapaksha.com>
2026-05-17 12:00:20 +0530

    Add monitor service

---
+++ internal/uaclient/monitor.go
@@ -0,0 +1,92 @@
+package uaclient
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/DishanRajapaksha/opc-ua-cli/internal/domain"
+	"github.com/gopcua/opcua"
+	"github.com/gopcua/opcua/monitor"
+)
+
+// Subscription wraps a server-side OPC UA subscription and exposes data changes as a channel.
+type Subscription struct {
+	Events <-chan domain.DataChange
+	Errors <-chan error
+	close  func()
+}
+
+func (s Subscription) Close() {
+	if s.close != nil {
+		s.close()
+	}
+}
+
+func (s *Service) Monitor(ctx context.Context, nodes []string, interval time.Duration) (Subscription, error) {
+	nodeMonitor, err := monitor.NewNodeMonitor(s.client)
+	if err != nil {
+		return Subscription{}, err
+	}
+
+	rawChanges := make(chan *monitor.DataChangeMessage, 32)
+	subscription, err := nodeMonitor.ChanSubscribe(ctx, &opcua.SubscriptionParameters{Interval: interval}, rawChanges, nodes...)
+	if err != nil {
+		return Subscription{}, err
+	}
+
+	events := make(chan domain.DataChange, 32)
+	errors := make(chan error, 8)
+
+	go func() {
+		defer close(events)
+		defer close(errors)
+		defer subscription.Unsubscribe(context.Background())
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case change, ok := <-rawChanges:
+				if !ok {
+					return
+				}
+				if change == nil {
+					continue
+				}
+				if change.Error != nil {
+					select {
+					case errors <- change.Error:
+					case <-ctx.Done():
+					}
+					continue
+				}
+
+				value := interface{}(nil)
+				if change.Value != nil {
+					value = change.Value.Value()
+				}
+
+				event := domain.DataChange{
+					NodeID:          fmt.Sprint(change.NodeID),
+					Value:           value,
+					SourceTimestamp: formatTimestamp(change.SourceTimestamp),
+				}
+
+				select {
+				case events <- event:
+				case <-ctx.Done():
+					return
+				}
+			}
+		}
+	}()
+
+	return Subscription{
+		Events: events,
+		Errors: errors,
+		close: func() {
+			subscription.Unsubscribe(context.Background())
+		},
+	}, nil
+}
